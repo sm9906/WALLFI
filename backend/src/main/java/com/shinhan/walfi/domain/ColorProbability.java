2725921732d945f83da32ecfcdbcdddb10bccb31
Heesom <heesom.hs@gmail.com>
2023-09-03 04:42:46 +0900

    [기능] ColorProbability 엔티티 생성
    - erd의 컬럼과 다른 컬럼명으로 변경 [percent -> color_percent]

---
+++ backend/src/main/java/com/shinhan/walfi/domain/ColorProbability.java
@@ -0,0 +1,16 @@
+package com.shinhan.walfi.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "color_probability")
+public class ColorProbability {
+    @Id
+    @Enumerated(EnumType.STRING)
+    private Color color;
+
+    private float color_percent;
+}
