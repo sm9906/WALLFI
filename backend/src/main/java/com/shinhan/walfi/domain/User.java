9f0bb0d798e4f5a8a0f37a98b082deed661be669
Heesom <heesom.hs@gmail.com>
2023-09-03 03:44:09 +0900

    [기능] User 엔티티 추가
    - erd에 있던 id 컬럼을 user_id로 표기
    - date format을 LocalDateTime 타입으로 사용

---
+++ backend/src/main/java/com/shinhan/walfi/domain/User.java
@@ -0,0 +1,30 @@
+package com.shinhan.walfi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private String id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private LocalDateTime birth_date;
+
+    private String phone_number;
+
+}
