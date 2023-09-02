d2e285ed1c70ab5552a16890ecffbf48b5909870
Heesom <heesom.hs@gmail.com>
2023-09-03 03:54:03 +0900

    [기능] Account 엔티티 추가
    - account_number 변수의 타입을 String으로 지정

---
+++ backend/src/main/java/com/shinhan/walfi/domain/Account.java
@@ -0,0 +1,27 @@
+package com.shinhan.walfi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Account {
+    @Id
+    private String account_number;
+
+    private int balance;
+
+    private LocalDateTime created_time;
+
+    private int account_password;
+
+    private String currency;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
