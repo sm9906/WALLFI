908807ad202676f146ab5e6613fe407f345c87f4
박민아 <icherom@naver.com>
2023-09-07 14:32:23 +0900

    [기능] 환율 정보 불러오기

--- frontend/src/features/wallet/walletscreens/ExchangeSearch.js
+++ frontend/src/features/wallet/walletscreens/ExchangeSearch.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-
+import axios from 'axios';
 
 export default function ExchangeSearch({navigation}){
   useLayoutEffect(()=>{
-    axios.get()
+    const exchangeRate =async()=> {await axios.get('http://192.168.100.210:8080/exchange/info',{
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(exchangeRate.data.data.exchangeDtoList)
+    }
   }, [])
 
   return(
     <View>
-      
+      <Text>ㅁㄴㅇㄹ</Text>
     </View>
   )
 }
