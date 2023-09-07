7b24572d4701a6cf3d44ffad64d1d805c31d85a4
박민아 <icherom@naver.com>
2023-09-07 17:04:41 +0900

    [기능] 환율 정보 보여주기
    - 1차로 데이터 거르기
    
    - 디자인 필요

--- frontend/src/features/wallet/walletscreens/ExchangeSearch.js
+++ frontend/src/features/wallet/walletscreens/ExchangeSearch.js
@@ -1,21 +1,57 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Background } from '../walletcomponents/CommonStyle';
 import axios from 'axios';
 
+function ShwoExchange({ntnCode:통화코드, exchangeRate:매매기준환율, compareDt:전일대비}){
+  return(
+    <>
+      <View>
+        <Text>{ntnCode}</Text>
+      </View>
+    </>
+  )
+}
+
 export default function ExchangeSearch({navigation}){
-  useLayoutEffect(()=>{
-    const exchangeRate =async()=> {await axios.get('http://192.168.100.210:8080/exchange/info',{
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log(exchangeRate.data.data.exchangeDtoList)
-    }
-  }, [])
+  const [exchanges, setExchages] = useState();
+  useEffect(()=>{
+    exchangeRate();
+  }, []);
+
+  const exchangeRate = async()=> {
+    const result = await axios.get('http://192.168.100.210:8080/exchange/info',{
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    });
+    setExchages([...result.data.data.exchangeDtoList]);
+  };
 
   return(
-    <View>
+    <View style={{...Background.background, justifyContent:'flex-start'}}>
+      <View style={{...styles.excontainer, marginTop:'15%'}}>
+        <ShwoExchange/>
+      </View>
+      <View style={styles.excontainer}>
+        
+      </View>
+      <Text>{exchanges&&exchanges.map((exchange)=>{
+        return exchange.매매기준환율
+      })}
+      </Text>
+      {/* </View> */}
       <Text>ㅁㄴㅇㄹ</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  excontainer:{
+    width:'85%',
+    height:'15%',
+    margin:'3%',
+    borderRadius: 15,
+    backgroundColor: '#E6F0FC'
+  }
+})
