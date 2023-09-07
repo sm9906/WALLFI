09b72d5d7a5bee56a9480966e05564d4fa5d66bf
진희솜 <heesom.hs@gmail.com>
2023-09-07 17:41:58 +0900

    [기능] CharacterService에서 캐릭터 타입을 랜덤으로 지정 후 캐릭터 생성하는 기능 추가

---
+++ backend/src/main/java/com/shinhan/walfi/service/CharacterService.java
@@ -0,0 +1,47 @@
+package com.shinhan.walfi.service;
+
+import com.shinhan.walfi.domain.CharacterType;
+import com.shinhan.walfi.domain.game.GameCharacter;
+import com.shinhan.walfi.domain.game.UserGameInfo;
+import com.shinhan.walfi.repository.CharacterRepository;
+import com.shinhan.walfi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CharacterService {
+
+    private final CharacterRepository characterRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 캐릭터 타입 랜덤 설정 후 캐릭터 생성
+     *
+     * @param userId
+     * @return 생성한 캐릭터 idx 반환
+     */
+    @Transactional
+    public Long create(String userId) {
+        UserGameInfo userGameInfo = userRepository.findUserGameInfo(userId);
+
+        Random random = new Random();
+
+        // 캐릭터 타입 랜덤 생성
+        CharacterType[] characterTypes = CharacterType.values();
+        int typesRandomNum = random.nextInt(characterTypes.length);
+        CharacterType randomCharacterType = characterTypes[typesRandomNum];
+
+        // 캐릭터 생성
+        GameCharacter gameCharacter = GameCharacter.createCharacter(userGameInfo, randomCharacterType);
+
+        // db에 저장
+        characterRepository.save(gameCharacter);
+
+        return gameCharacter.getCharacterIdx();
+    }
+}
