ec760826e617d15f30bb2330fe94e4fa1d0f4477
김민수 <mskim.spring@gmail.com>
2023-09-07 16:52:35 +0900

    [문서] add RequestTestController.java in gitignore

--- backend/src/main/java/com/shinhan/walfi/controller/TestController.java
+++ backend/src/main/java/com/shinhan/walfi/controller/TestController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping
     public String test(){
-        return "Test!!!";
+        return "서버 작동 중!";
     }
-
 }
