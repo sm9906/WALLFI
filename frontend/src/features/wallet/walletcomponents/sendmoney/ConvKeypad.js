ba2049e892308b38b7dd68e9e5564b8297ba4085
박민아 <icherom@naver.com>
2023-09-09 23:08:31 +0900

    [버그] 가상 키패드
    - 가상 키패드 무한 참조 오류 수정
    
    - 가상 키패드 렌더링 최적화

--- frontend/src/features/wallet/walletcomponents/sendmoney/ConvKeypad.js
+++ frontend/src/features/wallet/walletcomponents/sendmoney/ConvKeypad.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import { View, Text, TextInput, Image, ScrollView , TouchableOpacity, StyleSheet } from 'react-native';
+import VirtualKeyboard from './VirtualKeypad';
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../ScreenSize';
 
 const convInput = ['+1만', '+5만', '+10만', '+100만', '전액'];
 
-export const ConvPad = () => {
+export const ConvPad = React.memo(function ConvPad({addMoney}){
+  const pressConvBtn = (idx)=> {
+    let plus = 10000;
+    if(idx==3){
+      plus = 1000000;
+    }else if(idx==4){
+      // 전액 다 선택
+    }else{
+      plus=(idx!==0)?50000*idx:plus;
+    }
+    addMoney(plus);
+  }
   return(
+    <>
     <View style={styles.convPad}>{
       convInput.map((conv, index)=>{
         return(
-        <TouchableOpacity style={styles.convBtn} key={index}>
+        <TouchableOpacity style={styles.convBtn} key={index} onPress={()=>{pressConvBtn(index)}}>
           <Text style={styles.convFont}>{conv}</Text>
         </TouchableOpacity>)
       })}
-      
     </View>
+    <VirtualKeyboard color='black' pressMode='string' onPress={(val) => addMoney(val)} />
+    </>
   )
-}
+})
 
 const styles = StyleSheet.create({
   convPad:{
-    width: SCREEN_WIDTH,
+    width:'100%',
+    height:SCREEN_HEIGHT*0.04,
     flexDirection:'row',
-    height: SCREEN_HEIGHT*0.04,
     alignItems:'center',
     justifyContent:'space-around',
   },
--- frontend/src/features/wallet/walletscreens/SnedHow.js
+++ frontend/src/features/wallet/walletscreens/SnedHow.js
@@ -2,13 +2,26 @@ import React, {useState} from "react";
 import { View, Text, TextInput, Image, ScrollView , TouchableOpacity, StyleSheet } from 'react-native';
 import { ConvPad } from "../walletcomponents/sendmoney/ConvKeypad";
 import { RFPercentage } from "react-native-responsive-fontsize";
+import VirtualKeyboard from "../walletcomponents/sendmoney/VirtualKeypad";
 
 export default function SendHow({route, navigation}){
   const accountTo = route.params.account;
   const bankTo = route.params.bank;
 
-  const [money, setMoney] = useState(0);
+  const [money, setMoney] = useState('0');
 
+  const addMoney=(value)=>{
+    if(value===''){
+      value='0'
+    }
+    console.log(typeof(value))
+    if(typeof(value)==='number'){
+      value += Number(money);
+      console.log('ㅎㅇ')
+      value = String(value);
+    }
+    setMoney(value);
+  }
 
   return(
     <View style={styles.background}>
@@ -16,7 +29,7 @@ export default function SendHow({route, navigation}){
       <Text style={styles.infoText}>얼마를 보낼까요?</Text>
       <Text>{money}원</Text>
       <Text>신한 110-556-869686 0원</Text>
-      <ConvPad />
+      <VirtualKeyboard color='black' pressMode='string' onPress={(val) => addMoney(val)} />
     </View>
   )
 }
