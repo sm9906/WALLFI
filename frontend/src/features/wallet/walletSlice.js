477f8f060a6143c25f08d23d834b910b5f24bb33
박민아 <icherom@naver.com>
2023-09-10 01:12:45 +0900

    [기능] walletSlice 추가
    - initialState 추가

--- frontend/src/features/wallet/walletSlice.js
+++ frontend/src/features/wallet/walletSlice.js
@@ -1,15 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  name:'',
   cards:[], // 월렛 들어갈 때 카드 컴포넌트 받아오면서 저장.
 }
 
+// 처음 로그인 해서 계좌 불러오는 action
+
+// 카드 추가 로직 
 
 export const walletSlice = createSlice({
   name: 'wallet',
   initialState,
   reducers:{
-    
+    // 돈 추가, 빼는 로직 
   }
 })
 
