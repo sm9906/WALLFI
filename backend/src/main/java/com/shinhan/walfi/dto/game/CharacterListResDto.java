1b7d2445727fbc0041331ac34f4281f8dd19cfc7
Heesom <heesom.hs@gmail.com>
2023-09-10 12:56:25 +0900

    [리팩토링] CharacterResDto -> CharacterListResDto
    - 기존 클래스와의 클래스명 혼동으로 인해 명확성을 부여하고자 클래스명에 List를 추가

--- backend/src/main/java/com/shinhan/walfi/dto/game/CharacterListResDto.java
+++ backend/src/main/java/com/shinhan/walfi/dto/game/CharacterListResDto.java
@@ -1,9 +1,7 @@
 package com.shinhan.walfi.dto.game;
 
-import com.shinhan.walfi.domain.game.GameCharacter;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
@@ -11,9 +9,10 @@ import java.util.List;
 @Getter
 @ToString
 @Builder
-public class CharacterResDto {
+public class CharacterListResDto {
 
     String userId;
+
     List<CharacterDto> characterDtoList;
 
 }
