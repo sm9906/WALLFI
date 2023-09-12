5a0530d36c996f4e3661102e076fba72ed6d6a4a
김민수 <mskim.spring@gmail.com>
2023-09-12 11:10:41 +0900

    [기능] 원화 이체

--- backend/src/main/java/com/shinhan/walfi/service/BankServiceImpl.java
+++ backend/src/main/java/com/shinhan/walfi/service/BankServiceImpl.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+import static java.util.Objects.isNull;
+
 
 @Slf4j
 @Service
@@ -15,17 +19,73 @@ public class BankServiceImpl implements BankService {
     private final BankMapper bankMapper;
 
     /**
-     * 1. 출금 계좌 번호 조회
-     * 2. 입금 대표 계좌 번호 조회
-     * 3. 입금 세부 계좌 번호 조회
-     * 4. 출금 계좌에 이체 금액 이상의 돈이 있는지 확인
-     * 5. 출금 계좌에 이체 금액만큼 차감
-     * 6. 입금 계좌에 이체 금액만큼 입금
+     * 원화 이체 Logic
+     * <p>
+     * 1. 출금 계좌 번호 조회 <p>
+     * 2. 입금 대표 계좌 번호 조회 <p>
+     * 3. 입금 세부 계좌 번호 조회 <p>
+     * 4. 출금 계좌에 이체 금액 이상의 돈이 있는지 확인 <p>
+     * 5. 출금 계좌에 이체 금액만큼 차감 <p>
+     * 6. 입금 계좌에 이체 금액만큼 입금 <p>
      *
      * @param localTransferDTO
      */
     @Override
+    @Transactional
     public void localCurrencyTransferDTO(LocalTransferDTO localTransferDTO) {
 
+        boolean result;
+        final String CURRENCY_CODE = "KRW";
+
+        final long TRANSFER_MONEY = localTransferDTO.get이체금액();
+        final String DEPOSIT_MAIN_ACCOUNT_NUMBER = localTransferDTO.get입금계좌번호();
+        final String WITHDRAWAL_MAIN_ACCOUNT_NUMBER = localTransferDTO.get출금계좌번호();
+
+        log.debug("=== 출금 대표 계좌 번호 조회 중 ===");
+        result = bankMapper.findMainAccountNumber(WITHDRAWAL_MAIN_ACCOUNT_NUMBER);
+        if (!result) {
+            log.error("출금 대표 계좌가 조회 불가");
+            return;
+        }
+
+        log.debug("=== 입금 대표 계좌 번호 조회 중 ===");
+        result = bankMapper.findMainAccountNumber(DEPOSIT_MAIN_ACCOUNT_NUMBER);
+        if (!result) {
+            log.error("입금 대표 계좌가 조회 불가");
+            return;
+        }
+
+        log.debug("=== 출금 세부 계좌 번호 조회 중 ===");
+        final String WITHDRAWAL_SUB_ACCOUNT_NUMBER = bankMapper.findSubAccountNumberByCurrencyCode(
+                WITHDRAWAL_MAIN_ACCOUNT_NUMBER,
+                CURRENCY_CODE
+        );
+        if (isNull(WITHDRAWAL_SUB_ACCOUNT_NUMBER)) {
+            log.error("출금 세부 계좌 번호 조회 불가");
+        }
+
+
+        log.debug("=== 입금 세부 계좌 번호 조회 중 ===");
+        final String DEPOSIT_SUB_ACCOUNT_NUMBER = bankMapper.findSubAccountNumberByCurrencyCode(
+                DEPOSIT_MAIN_ACCOUNT_NUMBER,
+                CURRENCY_CODE
+        );
+        if (isNull(DEPOSIT_SUB_ACCOUNT_NUMBER)) {
+            log.error("입금 세부 계좌 번호 조회 불가");
+        }
+
+        log.debug("=== 출금 계좌에 이체 금액 이상의 돈이 있는지 확인 ===");
+        result = bankMapper.checkSufficientMoneyForTransfer(
+                WITHDRAWAL_SUB_ACCOUNT_NUMBER,
+                TRANSFER_MONEY
+        );
+        if (!result) {
+            log.error("잔액 부족");
+            return;
+        }
+
+        log.debug("{}원 송금 진행: {} -> {}", TRANSFER_MONEY, WITHDRAWAL_SUB_ACCOUNT_NUMBER, DEPOSIT_SUB_ACCOUNT_NUMBER);
+        bankMapper.withdrawTransferMoneyFromAccount(WITHDRAWAL_SUB_ACCOUNT_NUMBER, TRANSFER_MONEY);
+        bankMapper.depositTransferMoneyFromAccount(DEPOSIT_SUB_ACCOUNT_NUMBER, TRANSFER_MONEY);
     }
 }
