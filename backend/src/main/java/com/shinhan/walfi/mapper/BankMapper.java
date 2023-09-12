e770e5fb579a728434ab964fd5fb2ad770109159
김민수 <mskim.spring@gmail.com>
2023-09-12 10:45:30 +0900

    [리팩토링] 이체 Logic 중 일부 자료형 변경
    - LocalTransferDTO 이체금액 : String -> int
    - 대표 계좌 번호 조회 Return Type : int -> boolean
    - 출금 계좌에 이체 금액 이상의 돈이 있는지 조회 Return Type : int -> boolean

--- backend/src/main/java/com/shinhan/walfi/mapper/BankMapper.java
+++ backend/src/main/java/com/shinhan/walfi/mapper/BankMapper.java
@@ -14,13 +14,13 @@ public interface BankMapper {
      */
 
     // 대표 계좌 번호 조회
-    int findMainAccountNumber(String mainWithdrawalAccountNumber);
+    boolean findMainAccountNumber(String mainWithdrawalAccountNumber);
 
     // 세부 계좌 번호 통화 코드로 조회
     String findSubAccountNumberByCurrencyCode(String mainWithdrawalAccountNumber, String currencyCode);
 
     // 출금 계좌에 이체 금액 이상의 돈이 있는지 확인
-    int checkSufficientMoneyForTransfer(@Param("subAccountNumber") String subAccountNumber,
+    boolean checkSufficientMoneyForTransfer(@Param("subAccountNumber") String subAccountNumber,
                                         @Param("transferMoney") int transferMoney);
 
     // 출금 계좌에 이체 금액만큼 차감
