939d572e459aa8e021b1a5797eaa54687966727b
김민수 <mskim.spring@gmail.com>
2023-09-13 00:01:42 +0900

    테스트

--- backend/src/main/java/com/shinhan/walfi/controller/TestController.java
+++ backend/src/main/java/com/shinhan/walfi/controller/TestController.java
@@ -1,16 +1,40 @@
 package com.shinhan.walfi.controller;
 
+import com.shinhan.walfi.domain.banking.TestEntity;
+import com.shinhan.walfi.repository.TestRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestRepo testRepo;
 
     @GetMapping
     public String test(){
         return "서버 작동 중!";
+    }
+
+    @GetMapping("/test")
+    public TestEntity test1() {
+
+        TestEntity testEntity = new TestEntity(
+                1L,
+                "english",
+                "한글데이터",
+                "english_data",
+                "한글데이터"
+        );
+
+        testRepo.save(testEntity);
+
+        return testRepo.findById(1L).get();
     }
 }
