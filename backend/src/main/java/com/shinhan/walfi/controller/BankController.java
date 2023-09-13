c3063b7e3cf9728e426e8051cb7186ddb9754e93
김민수 <mskim.spring@gmail.com>
2023-09-13 16:35:38 +0900

    [버그] 원화 이체 수정

--- backend/src/main/java/com/shinhan/walfi/controller/BankController.java
+++ backend/src/main/java/com/shinhan/walfi/controller/BankController.java
@@ -1,13 +1,19 @@
 package com.shinhan.walfi.controller;
 
+import com.shinhan.walfi.domain.HttpResult;
 import com.shinhan.walfi.dto.transfer.LocalTransferDTO;
 import com.shinhan.walfi.service.banking.BankService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.shinhan.walfi.domain.HttpResult.Result.SUCCESS;
+import static org.springframework.http.HttpStatus.OK;
 
 
 @Slf4j
@@ -19,12 +25,13 @@ public class BankController {
     private final BankService bankService;
 
     @PostMapping("/localtransfer")
-    public LocalTransferDTO localTransfer(@RequestBody LocalTransferDTO localTransferDTO) {
+    public ResponseEntity<HttpResult> localTransfer(@RequestBody LocalTransferDTO localTransferDTO) {
 
         log.debug("원화 이체 Request: {}", localTransferDTO);
 
         bankService.localCurrencyTransferDTO(localTransferDTO);
 
-        return localTransferDTO;
+        HttpResult res = new HttpResult(OK, SUCCESS, "이체 성공");
+        return ResponseEntity.status(200).body(res);
     }
 }
