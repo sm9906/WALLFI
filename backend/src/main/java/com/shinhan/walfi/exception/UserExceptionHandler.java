8b3647fcf4360ba9d8d9e7713ab0c7ff970500ae
heesom <heesom.hs@gmail.com>
2023-09-13 13:43:00 +0900

    [리팩토링] UserExceptionHandler에 로그 추가

--- backend/src/main/java/com/shinhan/walfi/exception/UserExceptionHandler.java
+++ backend/src/main/java/com/shinhan/walfi/exception/UserExceptionHandler.java
@@ -2,6 +2,7 @@ package com.shinhan.walfi.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.shinhan.walfi.domain.HttpResult;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.JDBCException;
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import static com.shinhan.walfi.domain.HttpResult.Result.FAIL;
 import static org.springframework.http.HttpStatus.BAD_GATEWAY;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-
+@Slf4j
 @RestControllerAdvice
 public class UserExceptionHandler {
 
@@ -48,14 +49,10 @@ public class UserExceptionHandler {
         return convertFailCodeToHttpResponse(error.getUserErrorCode());
     }
 
+    @ExceptionHandler(JDBCConnectionException.class)
+    protected ResponseEntity<String> handleConnectionException(JDBCException error) {
+        log.error("=== db 커넥션 에러 ===");
+        return new ResponseEntity<>(BAD_GATEWAY);
+    }
+
 }
-//    @ExceptionHandler(JDBCConnectionException.class)
-//    protected ResponseEntity<String> handleConnectionException(JDBCException error) {
-//        return new ResponseEntity<>(BAD_GATEWAY);
-//    }
-//
-//    @ExceptionHandler(InvalidFormatException.class)
-//    protected ResponseEntity<HttpResult> handleInvalidFormatException(InvalidFormatException e) {
-//        HttpResult httpResult = new HttpResult(BAD_REQUEST, ERROR, "파라미터 값이 잘못 전달되었습니다.");
-//        return ResponseEntity.status(BAD_REQUEST).body(httpResult);
-//    }
