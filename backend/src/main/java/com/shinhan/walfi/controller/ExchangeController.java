c146ae776ca7243635c3e0cbb76762290839281f
Heesom <heesom.hs@gmail.com>
2023-09-14 22:14:50 +0900

    [기능] 환전 기능 추가

--- backend/src/main/java/com/shinhan/walfi/controller/ExchangeController.java
+++ backend/src/main/java/com/shinhan/walfi/controller/ExchangeController.java
@@ -2,14 +2,13 @@ package com.shinhan.walfi.controller;
 
 import com.shinhan.walfi.domain.HttpResult;
 import com.shinhan.walfi.dto.banking.ExchangeResDto;
+import com.shinhan.walfi.dto.banking.UserExchangeReqDto;
 import com.shinhan.walfi.service.banking.ExchangeService;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exchange")
@@ -30,6 +29,20 @@ public class ExchangeController {
         } catch (ParseException e) {
             res = new HttpResult(HttpStatus.FORBIDDEN, HttpResult.Result.ERROR, "환율 조회 실패");
         }
+
+        return ResponseEntity.status(res.getStatus()).body(res);
+    }
+
+    @PostMapping("/do")
+    public ResponseEntity<HttpResult> userExchange(@RequestBody UserExchangeReqDto userExchangeReqDto) {
+
+        exchangeService.userExchange(userExchangeReqDto.getUserId(),
+                userExchangeReqDto.get사용자대표계좌(),
+                userExchangeReqDto.get통화코드(),
+                userExchangeReqDto.get금액(),
+                userExchangeReqDto.get전신환매도환율());
+
+        HttpResult res = HttpResult.getSuccess();
 
         return ResponseEntity.status(res.getStatus()).body(res);
     }
