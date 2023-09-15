49a8714d197e9faed787b4aef214fb229fb34161
서지호 <cjsrhd882@naver.com>
2023-09-15 12:40:07 +0900

    병합 서지호 - 박민아

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ReactotronConfig';
-import store from './src/store/store'
+import store from './src/common/store'
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -18,7 +18,7 @@ export default function App() {
         <RootStack.Navigator initialRouteName='Auth' >
           <RootStack.Screen name="GoogleMap" component={GoogleMapNavigator} options={{ headerShown: false }} />
           <RootStack.Screen name="Fight" component={FightNavigator} options={{ headerShown: false }} />
-          <RootStack.Screen name="Wallet" component={WalletNavigation} />
+          <RootStack.Screen name="Wallet" component={WalletNavigation} options={{ headerShown: false }} />
           <RootStack.Screen name="Auth" component={AuthNavigation } options={{ headerShown: false }}/>
         </RootStack.Navigator>
       </NavigationContainer>
--- frontend/src/common/store.js
+++ frontend/src/common/store.js
@@ -1,17 +1,28 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
-
 import walletReducer from '../features/wallet/walletSlice'
 import authReducer from '../features/auth/authSlice';
+import cardReducer from '../reducers/cardReducer';
+import loadingReducer from '../reducers/loadingReducer';
+import turnReducer from '../reducers/turnReducer';
+import animalReducer from '../reducers/animalReducer';
+import Reactotron from '../../ReactotronConfig';
+import { createStore  } from 'redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   wallet:walletReducer,
-  auth: authReducer
+  auth: authReducer,
+  cardReducer: cardReducer,
+  loadingReducer: loadingReducer,
+  turnReducer: turnReducer,
+  animalReducer: animalReducer,
 })
 
 const store = configureStore({
-  reducer:rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }).concat(thunk),
   devTools: true,
-})
+  enhancers: [Reactotron.createEnhancer()]
+});
 
 export default store;
