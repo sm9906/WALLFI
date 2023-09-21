f209bdfa17245c60af25f8c996d8fd9f69f2976e
JeongE2 <kpo5248@daum.net>
2023-09-21 09:09:22 +0900

    [기능] 신한 -> 한국수출입은행 API로 교체
    - 요청 주소 및 변수 변경

--- backend/src/main/java/com/shinhan/walfi/util/ExchangeUtil.java
+++ backend/src/main/java/com/shinhan/walfi/util/ExchangeUtil.java
@@ -22,56 +22,47 @@ import java.util.List;
 @Component
 public class ExchangeUtil {
 
-    private final String baseUrl = "https://shbhack.shinhan.com";
+    private final String baseUrl = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=";
 
-    private final String apikey = "2023_Shinhan_SSAFY_Hackathon";
+    private final String apikey = "QVWvuzOjXchQ14bAZwWTdCDrzkYIAmBP";
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public List<ExchangeDto> getTodayExchange() throws ParseException { //오늘의 환율정보 리스트 반환
-        String url = baseUrl + "/v1/search/fxrate/number"; // 요청 url
+        StringBuilder url = new StringBuilder(baseUrl);
+        url.append(apikey);
+        url.append("&searchdate=");
         Date now = new Date(); // 오늘 날짜 6자리
-        SimpleDateFormat format = new SimpleDateFormat("YYMMdd");
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
         String today = format.format(now);
+        url.append(today);
+        url.append("&data=AP01");
 
-        HttpHeaders headers = new HttpHeaders(); // 헤더 생성
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Todo : 가능하면 String 대신 Json 객체로 body 구성
-        // 바디 생성
-        StringBuilder body = new StringBuilder();
-        body.append("{\n    \"dataHeader\": {\n     \"apikey\": \"");
-        body.append(apikey);
-        body.append("\"\n    },\n    \"dataBody\": {\n        \"조회일자\": \"");
-        body.append(today);
-        body.append("\"\n    }\n}");
-        HttpEntity<?> requestMessage = new HttpEntity<>(body.toString(), headers);
-        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
+        HttpEntity<String> response = restTemplate.getForEntity(url.toString(), String.class);
 
         String responseBody = response.getBody(); // response를 파싱하여 환율 정보만 추출
         JSONParser parser = new JSONParser();
-        JSONObject jsonBody = (JSONObject) parser.parse(responseBody);
-        JSONObject jsonDataBody = (JSONObject) jsonBody.get("dataBody");
-        JSONArray jsonExchangeList = (JSONArray) jsonDataBody.get("환율리스트"); // 환율 리스트 json 객체 뽑아냄
+        JSONArray jsonExchangeList = (JSONArray) parser.parse(responseBody);
 
         List<ExchangeDto> list = new ArrayList<>(); // json 객체를 Exchange list 객체로 파싱
-        int[] countryIdx = {0, 1, 2, 4, 10}; // 5개국에 대해서만 (미국 일본 유럽 호주 중국)
+        int[] countryIdx = {1,6,8,12,22}; // 5개국에 대해서만 (호주 중국 유럽 일본 미국 )
         for (int c = 0; c < countryIdx.length; c++) {
             JSONObject item = (JSONObject) jsonExchangeList.get(countryIdx[c]);
             list.add(this.makeExchangeDto(item)); // json 객체를 ExchangeDto로 파싱
+            if(c == 3){
+                list.get(3).set통화코드(list.get(3).get통화코드().substring(0,3));
+            }
         }
         return list;
     }
 
     private ExchangeDto makeExchangeDto(JSONObject item) { // json 객체를 ExchangeDto로 파싱
         ExchangeDto dto = ExchangeDto.builder().
-                통화명((String) item.get("통화CODE_DISPLAY")).
-                매매기준환율(Float.parseFloat(((String) item.get("매매기준환율")))).
-                통화코드((String) item.get("통화CODE")).
-                전신환매도환율(Float.parseFloat(((String) item.get("전신환매도환율")))).
-                전신환매입환율(Float.parseFloat(((String) item.get("전신환매입환율")))).
-                지폐매도환율(Float.parseFloat(((String) item.get("지폐매도환율")))).
-                지폐매입환율(Float.parseFloat(((String) item.get("지폐매입환율")))).
+                통화명((String) item.get("cur_nm")).
+                매매기준환율(Float.parseFloat(((String) item.get("deal_bas_r")).replaceAll(",",""))).
+                통화코드((String) item.get("cur_unit")).
+                전신환매도환율(Float.parseFloat(((String) item.get("ttb")).replaceAll(",",""))).
+                전신환매입환율(Float.parseFloat(((String) item.get("tts")).replaceAll(",",""))).
                 build();
         return dto;
     }
