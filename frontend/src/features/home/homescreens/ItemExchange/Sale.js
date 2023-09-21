974b5dbb6806884aea916e93b83c6e3ac1d762ff
sm9906 <suminkim0618@gmail.com>
2023-09-21 17:31:58 +0900

    [기능] 거래소 판매 UI 작성중

--- frontend/src/features/home/homescreens/ItemExchange/Sale.js
+++ frontend/src/features/home/homescreens/ItemExchange/Sale.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  StyleSheet 
+} from 'react-native';
+
+import ButtonGroup from './ButtonGroup.js';
+import Search from './Search.js';
 
 // 판매 버튼
 export default function Sale() {
 
+  const [selectedBtn, setSelectedBtn] = useState(false);
+
   return (
-    <Text style={{ flex: 7 }}>판매</Text>
+    <View style={styles.saleContainer}>
+      <ButtonGroup selectedBtn={selectedBtn} setSelectedBtn={setSelectedBtn} />
+      <Search />
+      <View style={styles.itemContainer}></View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  saleContainer: { 
+    flex: 7, 
+    width: '100%', 
+    alignItems: 'center'
+  },
+  itemContainer: { 
+    flex: 6, 
+    width: '80%', 
+    marginBottom: '5%' 
+  }
+})
