02f5257e5a6f9557932f6acc4454ae339d749e27
JeongE2 <kpo5248@daum.net>
2023-09-25 10:09:16 +0900

    [기능] 환율 조회 수정
    - 어제 날짜와 비교하는 것이 아닌 최근의 전날과 비교하도록 수정

--- backend/src/main/java/com/shinhan/walfi/repository/banking/ExchangeRepository.java
+++ backend/src/main/java/com/shinhan/walfi/repository/banking/ExchangeRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ExchangeRepository extends JpaRepository<ExchangeHistory, Long> {
-    @Query(value = "select * from exchange_history where 고시일자 = curdate() - interval 1 day order by 통화코드;", nativeQuery = true)
+    @Query(value = "select * from exchange_history order by 고시일자 desc, 통화코드 limit 5, 5;", nativeQuery = true)
     List<ExchangeHistory> findYesterday();
 
-    @Query(value = "select * from exchange_history where 고시일자 = curdate() order by 통화코드;", nativeQuery = true)
-    List<ExchangeHistory> findToday();
+//    @Query(value = "select * from exchange_history where 고시일자 = curdate() order by 통화코드;", nativeQuery = true)
+//    List<ExchangeHistory> findToday();
 
     @Query(value = "select * from exchange_history order by 고시일자 desc, 통화코드 limit 5;", nativeQuery = true)
     List<ExchangeHistory> getLatest();
