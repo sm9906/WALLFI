b90dfdeb509816b48fc62688536728532210635f
Heesom <heesom.hs@gmail.com>
2023-09-28 16:52:46 +0900

    [기능] 가상화폐 계좌 생성을 위한 개인키, 공개키 생성 기능

---
+++ backend/src/main/java/com/shinhan/walfi/util/CryptoUtil.java
@@ -0,0 +1,32 @@
+package com.shinhan.walfi.util;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class CryptoUtil {
+
+    public static String getSecretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128); // for example
+        SecretKey secretKey = keyGen.generateKey();
+        return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+    }
+
+    public static String encrypt(String strToEncrypt, String secret) {
+        try {
+            byte[] decodedKey = Base64.getDecoder().decode(secret);
+            SecretKeySpec secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+}
+
