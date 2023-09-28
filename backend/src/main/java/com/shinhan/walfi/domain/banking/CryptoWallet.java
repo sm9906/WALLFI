8cff0f51add474e5154a21d9a6643580bdfe58de
Heesom <heesom.hs@gmail.com>
2023-09-28 15:56:34 +0900

    [기능] CryptoWallet에 코인 타입 추가

--- backend/src/main/java/com/shinhan/walfi/domain/banking/CryptoWallet.java
+++ backend/src/main/java/com/shinhan/walfi/domain/banking/CryptoWallet.java
@@ -1,6 +1,7 @@
 package com.shinhan.walfi.domain.banking;
 
 import com.shinhan.walfi.domain.User;
+import com.shinhan.walfi.domain.enums.CoinType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,17 +26,21 @@ public class CryptoWallet {
 
     private String address;
 
+    @Enumerated(EnumType.STRING)
+    private CoinType coinType;
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "userId")
     private User user;
 
 
-    public CryptoWallet createCryptoWallet(String jsonWallet, String encpwd, String passwordKey, String address) {
+    public CryptoWallet createCryptoWallet(String jsonWallet, String encpwd, String passwordKey, String address, CoinType coinType) {
         CryptoWallet cryptoWallet = new CryptoWallet();
         cryptoWallet.jsonWallet = jsonWallet;
         cryptoWallet.encpwd = encpwd;
         cryptoWallet.passwordKey = passwordKey;
         cryptoWallet.address = address;
+        cryptoWallet.coinType = coinType;
 
         return cryptoWallet;
     }
---
+++ backend/src/main/java/com/shinhan/walfi/domain/enums/CoinType.java
@@ -0,0 +1,5 @@
+package com.shinhan.walfi.domain.enums;
+
+public enum CoinType {
+    ETHEREUM, SEPOLIA;
+}
